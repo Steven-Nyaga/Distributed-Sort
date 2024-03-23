dfbcdc30a806daed5c66ecff0564de2faf16b18f
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2024-03-23 12:02:25 +0100

    Firebase Initialization

---
+++ src/main/java/com/distributed/sort/distributedsort/firebase/FirebaseInitialization.java
@@ -0,0 +1,38 @@
+package com.distributed.sort.distributedsort.firebase;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.springframework.stereotype.Service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+@Service
+public class FirebaseInitialization {
+
+	public void initialization() {
+
+		try {
+			FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
+			FirebaseOptions options = new FirebaseOptions.Builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+
+			FirebaseApp.initializeApp(options);
+			
+		} catch (FileNotFoundException e) {
+			
+			e.printStackTrace();
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+
+	}
+
+}
