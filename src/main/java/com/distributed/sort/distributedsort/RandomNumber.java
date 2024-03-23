663e91c6724cc7c09b12e8e144e9f6b7933cb824
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2024-03-23 11:51:44 +0100

    Random number model class added

---
+++ src/main/java/com/distributed/sort/distributedsort/RandomNumber.java
@@ -0,0 +1,20 @@
+package com.distributed.sort.distributedsort;
+
+public class RandomNumber {
+	
+	private int Id;
+	private int randomNumber;
+	
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public int getRandomNumber() {
+		return randomNumber;
+	}
+	public void setRandomNumber(int randomNumber) {
+		this.randomNumber = randomNumber;
+	}
+}
