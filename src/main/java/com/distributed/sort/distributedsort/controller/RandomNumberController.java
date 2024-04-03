05358597819a78806ffd71c8fa3bd63c494ba182
Steven-Nyaga <37731180+Steven-Nyaga@users.noreply.github.com>
2024-04-03 12:50:04 +0200

    Fixed DB issues and configured mssql

--- src/main/java/com/distributed/sort/distributedsort/controller/RandomNumberController.java
+++ src/main/java/com/distributed/sort/distributedsort/controller/RandomNumberController.java
@@ -1,13 +1,23 @@
 package com.distributed.sort.distributedsort.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.distributed.sort.distributedsort.models.RandomNumber;
+import com.distributed.sort.distributedsort.service.RandomNumberSqlService;
 
 @RestController
 public class RandomNumberController {
+	
+	@Autowired
+	RandomNumberSqlService randomNumberSqlService;
 	
 	@PostMapping("/distributed-sort")
 	public String saveValue(@RequestBody RandomNumber randomNumber) {
@@ -22,5 +32,12 @@ public class RandomNumberController {
 			return "Error";
 		}
 	}
+	
+    @GetMapping("/data")
+    public Page<RandomNumber> getAllData(@RequestParam(defaultValue = "0") int page,
+                                            @RequestParam(defaultValue = "50") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return randomNumberSqlService.getAllData(pageable);
+    }
 	
 }
